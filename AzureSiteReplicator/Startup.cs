fa791c0c4ac61d5eb6c81c511d6008477fa372c2
davidebbo <david.ebbo@microsoft.com>
2013-11-10 19:30:44 -0800

    Initial commit

---
+++ AzureSiteReplicator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureSiteReplicator.Startup))]
+namespace AzureSiteReplicator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
